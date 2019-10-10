c30ddb3a1bae37032af6d2cbc4fe930ff9abc5ce
Kuldeep Tailor <43580220+tailorkuldeep@users.noreply.github.com>
2019-10-11 00:06:57 +0530

    update p5,py

--- p5.py
+++ p5.py
@@ -1,13 +1,5 @@
-l=[]
-cnt=0
-size=int(input("Enter size"))
-for i in range(0,size):
-    num=int(input("Enter element"))
-    l.append(num)
-print("List:",l)
-ch=int(input("Enter number to be count:"))
-for i in range(0,size):
-    if ch==l[i]:
-        cnt+=1
-print("Count of %d:"%ch,cnt)
-
+size=int(input("Enter size: "))
+l=list(map(int,input("Enter elements: ").split()))
+print("List: ",l)
+ch=int(input("Enter number to be count: "))
+print("Count of %d: "%ch,l[:size].count(ch))
